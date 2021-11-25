119fcf76eeb2eeb05a4cb4ff970ba7a44f2834bc
Guest1 <guest1@Ehmihlee.local>
2021-11-25 06:33:31 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,93 @@
+import { ReactComponent as LogoSvg} from './logo.svg';
 import React, { useState } from "react";
 import Osc1 from "./components/Osc1";
+import Filter from "./components/Filter";
 import './App.scss';
 
 //runs once, parent component- creates functions for updating it and pass them down to child components
 let actx = new AudioContext();
 let out = actx.destination;
 
+//The createOscillator() method of the BaseAudioContext interface creates an OscillatorNode, 
+//a source representing a periodic waveform. It basically generates a constant tone. 
 let osc1 = actx.createOscillator();
+//The createGain() method of the BaseAudioContext interface creates a GainNode, 
+//which can be used to control the overall gain (or volume) of the audio graph. 
 let gain1 = actx.createGain();
+// The createBiquadFilter() method of the BaseAudioContext interface creates a BiquadFilterNode, 
+// which represents a second order filter configurable as several different common filter types. 
+let filter = actx.createBiquadFilter();
 
 osc1.connect(gain1);
-gain1.connect(out);
-
+gain1.connect(filter);
+filter.connect(out);
 
 function App() {
+
+  //useState declarations
   const [osc1Settings, setOsc1Settings] = useState({
     frequency: osc1.frequency.value,
     detune: osc1.detune.value,
-    type: osc1.type
+    type: osc1.type,
   });
 
-  const changeOsc1 = e => {
+  const [filterSettings, setFilterSettings] = useState({
+    frequency: filter.frequency.value,
+    detune: filter.detune.value,
+    Q: filter.Q.value,
+    gain: filter.gain.value,
+    type: filter.type,
+  });
+
+  const changeOsc1 = (e) => {
     let { value, id } = e.target;
     setOsc1Settings({ ...osc1Settings, [id]: value });
     osc1[id].value = value;
   };
 
-  const changeOsc1Type = e => {
+  const changeOsc1Type = (e) => {
     let { id } = e.target;
-    setOsc1Settings({ ...osc1Settings, type: id })
+    //spread in current oscillator settings and put a key of type
+    setOsc1Settings({ ...osc1Settings, type: id });
     osc1.type = id;
   };
-change
+  //.type is not an object with a dot value
+  const changeFilter = (e) => {
+    let { value, id } = e.target;
+    setFilterSettings({ ...filterSettings, [id]: value });
+    filter[id].value = value;
+  };
+
+  const changeFilterType = (e) => {
+    let {id} = e.target;
+    setFilterSettings ({...filterSettings, type:[id]});
+    filter.type = id;
+    }
+
   return (
     <div className="App">
-      <h1>sliders</h1>
+      <div className="logo"> 
+        <LogoSvg/>
+      </div>
+      <div className="intro">
+      <h1>Welcome to PocketSound!</h1>
+      <h2>Sound exploration in your pocket.</h2>
+      <div className="wrapper">
+      <p>Waveforms are the starting point of synthesis—and of all sound. In a synthesizer, an oscillator creates a repeated signal of a waveform. 
+        This waveform is what gets altered through synthesis. When you're ready, click start to initiate the oscillator. Please be mindful of your volume.</p>
+        </div>
       <button onClick={() => osc1.start()}>Start</button>
       <button onClick={() => osc1.stop()}>Stop</button>
+      </div>
       <Osc1
         change={changeOsc1}
         settings={osc1Settings}
-        chaneType={changeOsc1Type}
+        changeType={changeOsc1Type}
       />
+      <Filter change={changeFilter} settings={filterSettings} changeType={changeFilterType} />
+      <footer>
+        <p>Created at Juno College 2021</p>
+      </footer>
     </div>
   );
 }
--- src/components/Osc1.js
+++ src/components/Osc1.js
@@ -1,16 +1,18 @@
 import React from "react"
 
 const Osc1 = ({ change, settings, changeType }) => {
-
+    // destructure type freq and detune from settings
+    const { type, frequency, detune } = settings;
+    
     return (
         <div className="control">
-            <h2>osc 1</h2>
+            <h2>Oscillator 1</h2>
 
             <div className="param">
                 {/* if range went from 0-100, the human ear wouldn't be able to hear it. The sub bass would be hard to hear to sub bass that's barely high enough. */}
                 <h3>Frequency</h3>
                 <input
-                    value={settings.frequency}
+                    value={frequency}
                     onChange={change}
                     max="5000"
                     type="range"
@@ -21,25 +23,21 @@ const Osc1 = ({ change, settings, changeType }) => {
             <div className="param">
                 <h3>Detune</h3>
                 <input
-                    value={settings.detune}
+                    value={detune}
                     onChange={change}
                     type="range"
                     id="detune"
                 />
             </div>
 
-
+            {/* check if ie. type is === sine, and if it is, include the class of active */}
             <div className="param">
                 <h3>Wave</h3>
-                <button id="sine" onClick={changeType} className={``}>Sine</button>
-                <button id="triangle" onClick={changeType} className={``}>Triangle</button>
-                <button id="square" onClick={changeType} className={``}>Square</button>
-                <button id="sawtooth" onClick={changeType} className={``}>Sawtooth</button>
+                <button onClick={changeType} id="sine"className={`${type === "sine" && "active"}`}>Sine</button>
+                <button onClick={changeType} id="triangle" className={`${type === "triangle" && "active"}`}>Triangle</button>
+                <button onClick={changeType} id="square" className={`${type === "square" && "active"}`}>Square</button>
+                <button onClick={changeType} id="sawtooth" className={`${type === "sawtooth" && "active"}`}>Sawtooth</button>
             </div>
-
-
-
-
         </div>
     );
 };
